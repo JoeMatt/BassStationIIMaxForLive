59d3f175f4687282e0757d69c451a68cb6486893
Joe Mattiello <git@joemattiello.com>
2014-08-27 21:48:47 -0400

    Used fancy Js to hack live.menu to do what menu was doing. This allows using keyboard buttons to switch patches. Minor detail but I like it. Also looks better. Also hid buttons from automation parameters export

---
+++ fixmenu.js
@@ -0,0 +1,51 @@
+
+// inlets and outlets
+inlets  = 1;
+outlets = 1;
+
+var items;
+
+function list (value) {
+	
+	if(arguments.length > 1) {
+		outlet(0, arguments);
+	}	
+}
+
+function clear() {
+	items = [];
+	cancelTimer();
+}
+
+function cancelTimer() {
+if (timeout !== null) {
+		timeout.cancel();
+		timeout = null;		
+	};
+}
+
+var timeout;
+function append(value) {
+
+		// cancel previous call if still around
+	cancelTimer();
+
+	timeout = new Task(send, this);
+	timeout.schedule(200);
+
+	items.push(value);
+}
+
+function send() {
+		// Send the list out
+	var command = "_parameter_range";
+
+		// Set the list
+	outlet(0, command, items.sort());
+	// messnamed("foo", command, items.sort());
+
+	outlet(0, 0);
+	
+	timeout = null;
+	items = [];
+}
